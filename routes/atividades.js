29eb8d5f4592dd96591cfc1c3a8320da4301b693
matheusyan-coder <84139856+matheusyan-coder@users.noreply.github.com>
2022-06-06 11:04:04 -0300

    Add files via upload

--- routes/atividades.js
+++ routes/atividades.js
@@ -1,3 +1,5 @@
+const atividades = require('../models/atividades')
+
 module.exports = (app)=>{
     app.post('/atividades', async(req,res)=>{
         //recuperando as informações digiadas
@@ -13,12 +15,49 @@ module.exports = (app)=>{
             tipo:dados.tipo,
             entrega:dados.entrega,
             instrucoes:dados.orientacao,
+            disciplina:dados.disciplina,
             usuario:dados.id
         }).save()
-
         //buscar todas as atividades desse ususário 
-        var buscar = await atividades.find({usuario:dados.id})
+        var buscar = await atividades.find({usuario:dados.id}) //esse find q gera os dados
         //console.log(buscar)
         res.render('atividades.ejs',{nome:dados.nome,id:dados.id,dados:buscar})
+    })
+
+    //excluir atividades
+    app.get("/excluir", async(req,res)=>{
+        //recuperar o parâmetro id da barra de endereço
+        var id = req.query.id
+        var excluir = await atividades.findOneAndRemove({ //pd ser findOneAndDelete tbm, faz same coisa
+            _id:id
+        })
+        //voltar para a página atividades
+        //res.render('atividades.ejs',{nome:dados.nome,id:dados.id,dados:buscar})
+        res.send("Atividade Excluída!!")
+    })
+
+    //alterar atividades
+    app.get("/alterar", async(req,res)=>{
+        //recuperar o parâmetro id da barra de endereço
+        var id = req.query.id
+        var alterar = await atividades.findOneAndReplace({ //pd ser findOneAndDelete tbm, faz same coisa
+            _id:id
+        })
+        //voltar para a página atividades
+        //res.render('atividades.ejs',{nome:dados.nome,id:dados.id,dados:buscar})
+        res.send("Atividade Alterada!!")
+    })
+
+    //entregar atividades
+    app.get("/entregue", async(req,res)=>{
+        //recuperar o parâmetro id da barra de endereço
+        var id = req.query.id
+        var entregue = await atividades.findOneAndRemove({ //pd ser findOneAndDelete tbm, faz same coisa
+            _id:id
+        })
+        
+        //voltar para a página atividades
+        //res.render('atividades.ejs',{nome:dados.nome,id:dados.id,dados:buscar})
+        res.send("Atividade Entregue!!")
     })
 }
